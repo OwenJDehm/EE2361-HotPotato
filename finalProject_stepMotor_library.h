671f18e09f6f3e2e50cdbedb82f9e4246602f553
Robert Chandler <137918777+rchan91@users.noreply.github.com>
2024-04-03 18:08:13 -0500

    Update finalProject_stepMotor_library.h

--- finalProject_stepMotor_library.h
+++ finalProject_stepMotor_library.h
@@ -1,10 +1,9 @@
 /* Date: April 26th, 2024
  * Name: Robert Chandler, Owen Dehm, Joe Thomas, Maverick Plsek
- * Student ID number: 5791316
  * Course number: EE 2361
  * Term: Spring 2024
  * Lab/assignment number: Final Project - Hot Potato Game
- * Created on March 24, 2024, 12:21 PM
+ * Created on March 24th, 2024, 12:21 PM
  * 
  * File Purpose: This is the header file, the header file contains all the 
  * the function declarations needed to be used in the main file. This file 
@@ -17,11 +16,11 @@
 #ifdef	__cplusplus
 extern "C" {
 #endif
-    void stepMotor_init(int num);
-    void stepMotor_setSpeed(long int speed);
+    void stepMotor_init();
     void stepMotor_movement(int steps);
+    void stepMotor_setSpeed(long int speed);
     void stepMotor(int this_step);
-    void delay_mus(unsigned int mus);
+    void delay_ms(unsigned int ms);
 #ifdef	__cplusplus
 }
 #endif
