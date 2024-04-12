6e5e68320d1d961617bd1c3dd6699d1e85159d41
OwenJDehm <126838736+OwenJDehm@users.noreply.github.com>
2024-04-11 20:12:17 -0500

    V2 of the Piezo Buzzer
    Piezo buzzer is now utilizing the PWM for output compare to drive the Piezo buzzer. We are no longer trying to manually toggle using the Timer 2 interrupt. We also moved the pin of the Piezo buzzer RP11 (physical pin 22) on the PIC24, as there were previous pin conflicts with the stepper motor library. In the wiring of the standalone Piezo Buzzer unit a status LED was added to ensure that the PIC 24 is receiving power. Also updated the main testing file (dehm0036_piezoMain.c) to include a testing of the tones, and varying the frequency of the tone, and including a simple scale.

--- dehm0036_piezoMain.c
+++ dehm0036_piezoMain.c
@@ -7,7 +7,7 @@
 
 #include "xc.h"
 #include "stdint.h"
-#include "dehm0036_piezo.h"
+#include "dehm0036_piezo_v2.h"
 
 // CW1: FLASH CONFIGURATION WORD 1 (see PIC24 Family Reference Manual 24.1)
 #pragma config ICS = PGx1          // Comm Channel Select (Emulator EMUC1/EMUD1 pins are shared with PGC1/PGD1)
@@ -41,16 +41,36 @@ void delay_ms(unsigned int ms) {
 
 int main() {
     pic24_init();
-    initPiezo();
     initTimer2();
-    setTone(20);
+    setTone(5999);
     
+    // for testing the Piezo Buzzer
     while(1) {
+        
+       setTone(5999);
        piezoOn();
-       delay_ms(200);
-       
+       delay_ms(250);
        piezoOff();
-       delay_ms(200);
+       delay_ms(50);
+       
+       setTone(2999);
+       
+       piezoOn();
+       delay_ms(250);
+       piezoOff();
+       delay_ms(1000);
+       
+       // down in tone
+       for (int i = 0; i < 20; i++) {
+           setTone(3500 + (i * 200));
+           
+           piezoOn();
+           delay_ms(100);
+           
+           piezoOff();
+           delay_ms(100);
+       }
+       delay_ms(1000);
     }
     return 0;
 }
