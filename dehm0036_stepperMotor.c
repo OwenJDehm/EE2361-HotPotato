f0860e4d4086813e9b479d960feed44bd6c7dd63
Robert Chandler <137918777+rchan91@users.noreply.github.com>
2024-04-23 14:15:45 -0500

    Update dehm0036_stepperMotor.c

--- dehm0036_stepperMotor.c
+++ dehm0036_stepperMotor.c
@@ -1,9 +1,25 @@
+/* Date: April 26th, 2024
+ * Name: Robert Chandler, Owen Dehm, Joe Thomas, Maverick Plsek
+ * Student ID number: 5791316
+ * Course number: EE 2361
+ * Term: Spring 2024
+ * Lab/assignment number: Final Project - Hot Potato Game
+ * Created on March 24, 2024, 12:22 PM
+ * 
+ * File Purpose: This is the source file, the source file contains all 
+ * of the function definitions that are used in the main file. This source
+ * file is only for the step motor and nothing else relating to the 
+ * Hot Potato Game Project.
+ */
+
 #include "xc.h"
 #include "stdint.h"
 
-volatile int stepsRemaining = 0;
+// Global Variables
+volatile int stepsRemaining = 0;        // 
 volatile int currentStep = 0;
 volatile int stepSequence = 1;
+
 
 void stepperMotor_init() {
     // set pins to output (RP2 through RP5)
@@ -17,6 +33,14 @@ void stepperMotor_init() {
     LATBbits.LATB3 = 0;
     LATBbits.LATB4 = 0;
     LATBbits.LATB5 = 0;
+
+    // Set up Timer 1
+    T1CON = 0x0000;     // stop timer 1 and reset control register
+    TMR1 = 0;           // clear timer 1
+    PR1 = 15999;
+    IFS0bits.T1IF = 0;  // clear interrupt flag
+    IEC0bits.T1IE = 1;  // enable interrupt
+    T1CONbits.TCKPS = 0b01; // 1 pre-scalar, may need to lower
 }
 
 void stepSpeed(int speed) {
@@ -76,21 +100,14 @@ void __attribute__((interrupt, no_auto_psv)) _T1Interrupt(void) {
     }
 }
 
-// setting up the Timer 1
-void init_timer1() {
-    T1CON = 0x0000;     // stop timer 1 and reset control register
-    TMR1 = 0;           // clear timer 1
-    PR1 = 15999;
-    IFS0bits.T1IF = 0;  // clear interrupt flag
-    IEC0bits.T1IE = 1;  // enable interrupt
-    T1CONbits.TCKPS = 0b01; // 1 pre-scalar, may need to lower
-    T1CONbits.TON = 1;  // start Timer 1
-}
-
-void stepMotor(int step) {
+void setMovement(int step) {
     stepsRemaining = step;     // set the step count
 }
 
 void stopMotor(){
     T1CONbits.TON = 0;
+}
+
+void startMotor(){
+    T1CONbits.TON = 1;
 }
