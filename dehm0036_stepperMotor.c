271d871ca0df7e6529a0c61da45619cbe2a38d10
Owen Dehm <126838736+OwenJDehm@users.noreply.github.com>
2024-04-23 13:43:33 -0500

    Stepper Motor V2
    made changes to the original stepper motor library to include a way to make the motor rumble and turn

---
+++ dehm0036_stepperMotor.c
@@ -0,0 +1,109 @@
+#include "xc.h"
+#include "stdint.h"
+
+volatile int stepsRemaining = 0;
+volatile int currentStep = 0;
+volatile int stepSequence = 1;
+
+void stepperMotor_init() {
+    // set pins to output (RP2 through RP5)
+    TRISBbits.TRISB2 = 0;
+    TRISBbits.TRISB3 = 0;
+    TRISBbits.TRISB4 = 0;
+    TRISBbits.TRISB5 = 0;
+    
+    // make sure none of the pins are on
+    LATBbits.LATB2 = 0;
+    LATBbits.LATB3 = 0;
+    LATBbits.LATB4 = 0;
+    LATBbits.LATB5 = 0;
+}
+
+void stepSpeed(int speed) {
+    if (speed == 0) {
+        T1CONbits.TON = 0; // stop timer 1
+    }
+    else {
+        int period = 40000000 / (256 * speed);
+        // not including a cap speed so we can over speed the motor to make it rumble
+        // from the gear not "snapping" to the inductors
+        PR1 = period;
+        T1CONbits.TON = 1; // resume timer 1
+    }
+}
+
+void __attribute__((interrupt, no_auto_psv)) _T1Interrupt(void) {
+    IFS0bits.T1IF = 0; // clear interrupt
+    // will use masking and bit shifting to clear and set to next steps
+    // going forward
+    
+    switch(stepSequence) {
+        case 0:
+            LATBbits.LATB2 = 1;
+            LATBbits.LATB3 = 0;
+            LATBbits.LATB4 = 1;
+            LATBbits.LATB5 = 0;   
+        break;
+        case 1:
+            LATBbits.LATB2 = 0;
+            LATBbits.LATB3 = 1;
+            LATBbits.LATB4 = 1;
+            LATBbits.LATB5 = 0;  
+        break;
+        case 2:
+            LATBbits.LATB2 = 0;
+            LATBbits.LATB3 = 1;
+            LATBbits.LATB4 = 0;
+            LATBbits.LATB5 = 1;             
+        break;
+        case 3:
+            LATBbits.LATB2 = 1;
+            LATBbits.LATB3 = 0;
+            LATBbits.LATB4 = 0;
+            LATBbits.LATB5 = 1;             
+        break;
+    }
+    
+    if (stepsRemaining != 0) {
+        if (stepsRemaining > 0) {
+            stepSequence = (stepSequence + 1) % 4;
+            stepsRemaining--;
+        }
+        else {
+            stepSequence = (stepSequence + 3) % 4;
+            stepsRemaining++;
+        }
+    }
+    /*
+    if (stepsRemaining > 0) { // steps still remaining so we need to step
+        LATB &= ~(0xF << 2); // clear last state
+        LATB |= (stepSequenceForward[currentStep] << 2);    // shift to new state of coils moving forward
+        
+        currentStep = (currentStep + 1) % 4;
+        // go to next step, the % 4 accounts for overflow
+        stepsRemaining--;
+    }*/
+    /*else if (stepsRemaining < 0) { // steps still reaming so we need to step (reverse)
+        LATB &= ~(0xF << 2); // clear last state
+        LATB |= (stepSequenceBackward[currentStep] << 2);   // shift to new state of coils moving backward
+        
+        currentStep = (currentStep + 3) % 4; // making sure always positive by adding 3 instead of -1
+        // go to next step, the % 4 accounts for overflow
+        stepsRemaining++;
+    }*/
+}
+
+// setting up the Timer 1
+void init_timer1() {
+    T1CON = 0x0000;     // stop timer 1 and reset control register
+    TMR1 = 0;           // clear timer 1
+    PR1 = 15999;
+    IFS0bits.T1IF = 0;  // clear interrupt flag
+    IEC0bits.T1IE = 1;  // enable interrupt
+    T1CONbits.TCKPS = 0b01; // 1 pre-scalar, may need to lower
+    T1CONbits.TON = 1;  // start Timer 1
+}
+
+void stepMotor(int step) {
+    stepsRemaining = step;     // set the step count
+}
---
+++ dehm0036_stepperMotor.h
@@ -0,0 +1,27 @@
+/* 
+ * File:   dehm0036_stepperMotor.h
+ * Author: owenj
+ *
+ * Created on April 12, 2024, 6:23 PM
+ */
+
+#ifndef DEHM0036_STEPPERMOTOR_H
+#define	DEHM0036_STEPPERMOTOR_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+    void stepperMotor_init();
+    void stepSpeed(int speed);
+    void init_timer1();
+    void stepMotor(int step);
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* DEHM0036_STEPPERMOTOR_H */
+
